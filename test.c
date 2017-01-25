8fc08094a4fe1c8f52781c93ff38ad67af6b13bd
Bence <bence1233@gmail.com>
2017-01-25 18:18:45 +0100

    Update test.c

--- test.c
+++ test.c
@@ -9,33 +9,7 @@
 #define ONE_WIRE_BUS 9
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-#define LOGO16_GLCD_HEIGHT 16 
-#define LOGO16_GLCD_WIDTH  16 
-static const unsigned char PROGMEM logo16_glcd_bmp[] =
-{ B00000000, B11000000,
-  B00000001, B11000000,
-  B00000001, B11000000,
-  B00000011, B11100000,
-  B11110011, B11100000,
-  B11111110, B11111000,
-  B01111110, B11111111,
-  B00110011, B10011111,
-  B00011111, B11111100,
-  B00001101, B01110000,
-  B00011011, B10100000,
-  B00111111, B11100000,
-  B00111111, B11110000,
-  B01111100, B11110000,
-  B01110000, B01110000,
-  B00000000, B00110000 };
+
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
@@ -51,12 +25,15 @@ void setup() {
   sensors.begin(); // Print a message to the LCD.
   int deviceCount=sensors.getDeviceCount(); /*Testing on startup*/
   lcd.print(deviceCount);
-  display.clearDisplay()
+  display.clearDisplay();
   
-  display.setTextSize(1);
+  /*display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  display.println("Hello, world!");
+  display.println("Kulso: ");
+  display.display();
+  delay(1000);
+  display.clearDisplay();*/
 }
 
 void loop() {
@@ -70,7 +47,7 @@ void loop() {
   lcd.print("TMP2: "); lcd.print(sensors.getTempCByIndex(2));
   lcd.setCursor(12,1); lcd.print("C");
   */
-  lcd.setCursor(0,0); // Outside temperature
+  /*lcd.setCursor(0,0); // Outside temperature
   float temp=sensors.getTempCByIndex(0);
   lcd.print("Kulso: ");
   if(temp-(int)temp<0.5) lcd.print((int)temp);
@@ -84,9 +61,30 @@ void loop() {
   else lcd.print((int)tempA+1);
   lcd.setCursor(10,1);
   lcd.print("C");
-  lcd.setCursor(0,3);
+  lcd.setCursor(0,3);*/
+  
   /*int deviceCount = sensors.getDeviceCount();
   lcd.print(deviceCount);*/
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  float temp=sensors.getTempCByIndex(0);
+  display.print("Kulso: ");
+  if(temp-(int)temp<0.5) display.print((int)temp);
+  else display.print((int)temp+1);
+  display.setCursor(10,0); display.print("C");
+
+  display.setCursor(0,1); //Average of inside temperature
+  float tempA = ((sensors.getTempCByIndex(1)+sensors.getTempCByIndex(2))/2);
+  display.print("Belso: ");
+  if(tempA-(int)tempA<0.5) display.print((int)tempA);
+  else display.print((int)tempA+1);
+  display.setCursor(10,1); display.print("C");
   
+  display.display();
   delay(1000);
+  display.clearDisplay();
+  
+  //delay(1000);
 }
