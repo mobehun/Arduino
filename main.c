7143c1a0e25d67bf434a7bc4a7b3fc5345676076
Bence <bence1233@gmail.com>
2017-01-26 12:08:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,76 +1,67 @@
 #include <LiquidCrystal.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include "RTClib.h"
 
 #define ONE_WIRE_BUS 9
-#define OLED_RESET 4
-Adafruit_SSD1306 display(OLED_RESET);
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 LiquidCrystal lcd(5, 7, 10, 11, 12, 13);
-
-const int button = 2;
-bool bState;
+RTC_DS1307 rtc;
 
 void setup() {
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
   lcd.begin(20, 4); // set up the LCD's number of columns and rows:
   sensors.begin(); // Print a message to the LCD.
-  int deviceCount=sensors.getDeviceCount(); /*Testing on startup*/
-  lcd.print(deviceCount);
-  display.clearDisplay();
-
-  pinMode(button,INPUT);
+  rtc.begin();
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 void loop() {
   sensors.requestTemperatures(); // Send the command to get temperatures
+  
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
 
-  lcd.setCursor(0,0); // Outside temperature
-  float temp=sensors.getTempCByIndex(0);
+  lcd.setCursor(0+6,0); // Outside temperature
   lcd.print("Kulso: ");
+  float temp=sensors.getTempCByIndex(0);
   if(temp-(int)temp<0.5) lcd.print((int)temp);
   else lcd.print((int)temp+1);
-  if(temp<-9){
-    lcd.setCursor(11,0); lcd.print("C");
-  }
-  else {
-    lcd.setCursor(10,0); lcd.print("C");
-  }
+  if(temp<-9){ lcd.setCursor(11+6,0); lcd.print("C"); }
+  else { lcd.setCursor(10+6,0); lcd.print("C"); }
 
-    
-  lcd.setCursor(0,1); //Average of inside temperature
-  float tempA = ((sensors.getTempCByIndex(1)+sensors.getTempCByIndex(2))/2);
+  lcd.setCursor(0+6,1); //Average of inside temperature
   lcd.print("Belso: ");
-  if(tempA-(int)tempA<0.5) lcd.print((int)tempA);
-  else lcd.print((int)tempA+1);
-  lcd.setCursor(10,1);
+  temp = ((sensors.getTempCByIndex(1)+sensors.getTempCByIndex(2))/2);
+  if(temp-(int)temp<0.5) lcd.print((int)temp);
+  else lcd.print((int)temp+1);
+  lcd.setCursor(10+6,1);
   lcd.print("C");
-  lcd.setCursor(0,3);
-  
-  int deviceCount = sensors.getDeviceCount();
-  display.setCursor(0,30);
-  display.print("Devices: ");
-  display.print(deviceCount);
 
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
+  delay(5000);
+  /*
+  lcd.setCursor(0,3);
+  int deviceCount = sensors.getDeviceCount();
+  lcd.print(deviceCount);*/
   
-  if(digitalRead(button) == HIGH){
-    bState=true;
-  }
-  else bState=false;
-  
-  display.display();
-  delay(500);
-  display.clearDisplay();
-  
-  //delay(1000);
+  DateTime ido = rtc.now();
+  lcd.clear();
+  lcd.print("Date: ");
+  lcd.setCursor(6,0);
+  lcd.print(ido.year());
+  lcd.print('/');
+  lcd.print(ido.month());
+  lcd.print('/');
+  lcd.print(ido.day());
+  lcd.setCursor(0,1);
+  lcd.print("Time: ");
+  lcd.setCursor(6,1);
+  lcd.print(ido.hour());
+  lcd.print(':');
+  lcd.print(ido.minute());
+  delay(5000);
+  lcd.clear();
 }
