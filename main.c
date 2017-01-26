41e2de8bb42abc8e3c58d27a10bcc7b897e27344
Bence <bence1233@gmail.com>
2017-01-26 10:56:10 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,13 +10,12 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-#if (SSD1306_LCDHEIGHT != 64)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
-
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 LiquidCrystal lcd(5, 7, 10, 11, 12, 13);
+
+const int button = 2;
+bool bState;
 
 void setup() {
   Serial.begin(9600);
@@ -25,26 +24,26 @@ void setup() {
   sensors.begin(); // Print a message to the LCD.
   int deviceCount=sensors.getDeviceCount(); /*Testing on startup*/
   lcd.print(deviceCount);
-  delay(1000);
+  display.clearDisplay();
+
+  pinMode(button,INPUT);
 }
 
 void loop() {
   sensors.requestTemperatures(); // Send the command to get temperatures
-  /*
-  lcd.setCursor(0,0); // Inside temperature No.1
-  lcd.print("TMP1: "); lcd.print(sensors.getTempCByIndex(1));
-  lcd.setCursor(12,0); lcd.print("C");
-  
-  lcd.setCursor(0,1); // Inside temperature No.2
-  lcd.print("TMP2: "); lcd.print(sensors.getTempCByIndex(2));
-  lcd.setCursor(12,1); lcd.print("C");
-  
+
   lcd.setCursor(0,0); // Outside temperature
   float temp=sensors.getTempCByIndex(0);
   lcd.print("Kulso: ");
   if(temp-(int)temp<0.5) lcd.print((int)temp);
   else lcd.print((int)temp+1);
-  lcd.setCursor(10,0); lcd.print("C");
+  if(temp<-9){
+    lcd.setCursor(11,0); lcd.print("C");
+  }
+  else {
+    lcd.setCursor(10,0); lcd.print("C");
+  }
+
     
   lcd.setCursor(0,1); //Average of inside temperature
   float tempA = ((sensors.getTempCByIndex(1)+sensors.getTempCByIndex(2))/2);
@@ -56,26 +55,21 @@ void loop() {
   lcd.setCursor(0,3);
   
   int deviceCount = sensors.getDeviceCount();
-  lcd.print(deviceCount);*/
+  display.setCursor(0,30);
+  display.print("Devices: ");
+  display.print(deviceCount);
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
-  float temp=sensors.getTempCByIndex(0);
-  display.print("Kulso: ");
-  if(temp-(int)temp<0.5) display.print((int)temp);
-  else display.print((int)temp+1);
-  display.setCursor(10,0); display.print("C");
-
-  display.setCursor(0,1); //Average of inside temperature
-  float tempA = ((sensors.getTempCByIndex(1)+sensors.getTempCByIndex(2))/2);
-  display.print("Belso: ");
-  if(tempA-(int)tempA<0.5) display.print((int)tempA);
-  else display.print((int)tempA+1);
-  display.setCursor(10,1); display.print("C");
+  
+  if(digitalRead(button) == HIGH){
+    bState=true;
+  }
+  else bState=false;
   
   display.display();
-  delay(1000);
+  delay(500);
   display.clearDisplay();
   
   //delay(1000);
